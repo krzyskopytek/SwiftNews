c69cbbb2d11ffa5c64a855d41460119b649f1585
krzyskopytek <krzys.kopytek@hotmail.com>
2020-09-18 13:29:18 -0400

    Initial Commit

---
+++ SwiftNews/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  SwiftNews
+//
+//  Created by Krzysztof Kopytek on 2020-09-18.
+//  Copyright © 2020 Kopytek. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
